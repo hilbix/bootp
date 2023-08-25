7e6c4d405725e84574c58761d31d4aa19c3e9fa0
Valentin Hilbig <webmaster@scylla-charybdis.com>
2023-08-25 02:19:44 +0000

    autostart/bootp.sh modified

--- autostart/bootp.sh
+++ autostart/bootp.sh
@@ -3,8 +3,14 @@
 me="$(readlink -e -- "$0")" || exit
 cd "${me%/*/*}" || exit
 
+DEV="${0##*/}"
+DEV="${DEV%.sh}"
+DEV="${DEV#"${DEV%-*}"}"
+DEV="${DEV#-}"
+
 PATH=$PATH:/usr/sbin:/sbin
-while	printf '\n%(%Y%m%d-%H%M%S)T rc=%d %q\n' -1 "$?" "$PWD" && ! read -t1
+while	printf '\n%(%Y%m%d-%H%M%S)T rc=%d %q %q\n' -1 "$?" "$PWD" "$DEV" && ! read -t1
 do
-	run-until-change bootp -- ./bootp vmbr0
+	# see https://github.com/hilbix/run-until-change
+	run-until-change bootp -- ./bootp "${DEV:-vmbr0}"
 done
