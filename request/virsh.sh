526d7350cde436e656fb2707998e09fc447fdfdd
Valentin Hilbig <webmaster@scylla-charybdis.com>
2023-09-08 23:04:38 +0200

    request/virsh.sh added

---
+++ request/virsh.sh
@@ -0,0 +1,30 @@
+#
+# vim: ft=bash
+#
+# Virsh VM autodetection
+#
+# Walks the snapshots from current up all parents
+
+[ -d /etc/libvirt/qemu ] || return
+
+virsh()		{ LC_ALL=C.UTF-8 exec /usr/bin/virsh "$@"; }
+snapfirst()	{ virsh snapshot-current "$VM" | xml2json/xml2json.py; }
+snapinfo()	{ jq -r .domainsnapshot.description <<<"$1"; }
+snapnext()
+{
+  snap="$(jq -r .domainsnapshot.name <<<"$1")" &&
+  snap="$(virsh snapshot-parent "$VM" --snapshotname "$snap")" &&
+  virsh snapshot-dumpxml "$VM" "$snap" | xml2json/xml2json.py
+}
+
+request()
+{
+  while read -ru6 VM
+  do
+        virsh domiflist "$VM" | grep -s " $MAC\$" || continue
+        walksnaps snapfirst snapinfo snapnext
+        return
+  done 6< <(virsh list --uuid --state-running)
+  return 1
+}
+
