0a22136ccb94b719c518c119e2b159433df9b199
Valentin Hilbig <webmaster@scylla-charybdis.com>
2023-09-09 02:59:37 +0200

    single caching

--- request.sh
+++ request.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #
 # This is an example bootp script.
-# request/*.sh are the scripts to autodetect the IP
+# request/*.sh are the scripts to autodetect the IP.
+#
+# ip/$IP.sh are additional scripts to parse and augment according to $IP
+# (see also ./preseed.sh)
 #
 # This needs "jq", so: sudo apt install jq
 
@@ -22,6 +25,8 @@ IP_ASSIGNED="${10}"	# packet: assigned IP (i.E. from proxied answers)
 IP_TFTP="${11}"		# packet: TFTP IP (next hop)
 IP_GW="${12}"		# packet: Gateway IP (forwarder)
 
+[ -d cache ] || mkdir cache
+
 bye()
 {
   echo ADDR 0.0.0.0	# setting 0 as address suppresses answer
@@ -41,7 +46,7 @@ bye()
 # returns 0 if found and data was output (else nothing is output)
 walksnaps()
 {
-  local SNAP tag data
+  local SNAP tag data vals more
 
   SNAP="$($1)"
   while	[ -n "$SNAP" ] || return
@@ -49,9 +54,11 @@ walksnaps()
         do
               #printf '%q %d %q\n' "$VM" "$tag" "$data" >&2
               case "$tag" in
-              (SEED|FILE)	l="${tag,,}"; [ -n "${!l}" ] || eval "$l=\"\$data\"";;
               (DHCP)		dhcp+=("$data");;
-              (IPv4)		ip="$data";;
+              (IPv4)		IP="$data";;
+              (*[^A-Z0-9_]*)	;;
+              (_[A-Z]*)		printf -vmore '_%q=%q %s' "$tag" "$data" "$more";;		# custom _VARs
+              (*)		tag="_$tag"; [ -n "${!tag}" ] || eval "$tag=\"\$data\"";;	# standard VARs
               esac
         done 6< <($2 "$SNAP")
 
@@ -62,43 +69,13 @@ walksnaps()
         SNAP="$($3 "$SNAP")"
   done
 
-  IP="$ip"
-  FILE="$file"
-  SEED="$seed"
+  [ -z "$more" ] || eval "$more"
   [ 0 = "${#dhcp[@]}" ] || printf 'DHCP %s\n' "${dhcp[@]}"
 }
 
-for a in request/*.sh
-do
-        # '' for default
-        IP=		# MUST [ip4] interface IPv4.  '-': script did it, 0.0.0.0 to ignore this MAC
-        MASK=		# OPT: [mask] interface netmask, either /CIDR or 255.x.y.z
-        GW=		# OPT: [ip4] default router
-        BC=		# OPT: [ip4] Broadcast address, default taken from interface
-        RENEW=		# OPT: [s] until RENEWING,  default  900000
-        LEASE=		# OPT: [s] for LEASE time,  default 1000000
-        REBIND=		# OPT: [s] until REBINDING, default 1500000
-        TFTP=		# OPT: [ip4] TFTP, default: GW
-        FILE=		# OPT: [path] BOOTP-Path
-        SEED=		# OPT: [word] http://$GW/d-i/$SEED/preseed.cfg
-        SECS=		# OPT: [s] seconds since boot
-        FLAG=		# OPT: flags
-        REPL=		# OPT: "port IP" or "0 IP" (for default port) reply to address
-
-        . "$a" &&	# import request()
-        request "$@" &&	# run request()
-        [ -n "$IP" ] &&
-        break
-done
-case "$IP" in
-(-)		exit;;	# script did everything
-('')		bye no matching VM found 'for' "$MAC";;
-(0.0.0.0)	bye ignoring "$MAC";;
-esac
-
 run()
 {
-  local -n ___VAR___="$1"
+  local -n ___VAR___="$1"	# this is correct, no _ here
 
   [ -n "$___VAR___" ] && return
   ___VAR___="$("${@:2}")" || bye $?: run "$@"
@@ -106,36 +83,12 @@ run()
 
 def()
 {
-  local -n ___VAR___="$1"
+  local -n ___VAR___="_$1"	# this is correct, _ added in front
 
   [ -n "$___VAR___" ] && return
   [ 2 -ge $# ] || printf -v ___VAR___ ' %q' "${@:3}"
   printf -v ___VAR___ %s%s "$2" "$___VAR___"
 }
-
-run	GW_J_R	ip -j r g "$IP"
-run	GW_IP	jq -r '.[0].prefsrc' <<<"$GW_J_R"
-run	GW_DEV	jq -r '.[0].dev' <<<"$GW_J_R"
-run	GW_J_A	ip -j a s "$GW_DEV"
-run	GW_CIDR	jq -r '.[].addr_info[] | select(.family=="inet").prefixlen' <<<"$GW_J_A"
-def	MASK	"/$GW_CIDR"
-def	GW	"$GW_IP"
-def	TFTP	"$GW"
-def	SEED	''
-def	FILE	"${SEED:+http://$GW/d-i/$SEED/preseed.cfg}"
-def	REPL	''
-def	FLAG	0
-def	SECS	0
-def	RENEW	 900000
-def	LEASE	1000000
-def	REBIND	1500000
-case "$MASK" in
-(/*)	bits=$[ 0xffffffff << (32 - "${MASK#/}") ];
-        printf -v MASK '%d.%d.%d.%d' $[ (bits>>24)&0xff ] $[ (bits>>16)&0xff ] $[ (bits>>8)&0xff ] $[ (bits>>0)&0xff ];
-        ;;
-esac
-IFS=. read a b c d u v w x <<<"$IP.$MASK" && printf -v BCDEF %d.%d.%d.%d $[ a|(u^255) ] $[ b|(v^255) ] $[ c|(w^255) ] $[ d|(x^255) ]
-def	BC	"$BCDEF"
 
 out()
 {
@@ -147,34 +100,115 @@ out()
 
 # XXX TODO XXX verify DHCP request for validity
 
+arp()
 {
-echo "$NAME $GW_IP IF $INTERFACE MAC $MAC IP $IP"
-arp -d "$IP"
-arp -s -i "$INTERFACE" "$IP" "$ARP" temp
-} >&2
+  echo "$NAME $GW_IP IF $INTERFACE MAC $MAC IP $IP"
+  /usr/sbin/arp -d "$IP"
+  /usr/sbin/arp -s -i "$INTERFACE" "$IP" "$ARP" temp
+}
 
-out REPL "$REPL"
-out ADDR "$IP"			# Set the IP of the VM
-out TFTP "$TFTP"		# Set our interface as TFTP server
-out HOST "$GW_IP"		# output BOOTP/DHCP server IP
-out FILE "$FILE"		# output boot file
-out SECS "$SECS"
-out FLAG "$FLAG"
+request()
+{
+  # All cachable variables, except IP, start with _
+  for a in request/*.sh
+  do
+        # '' for default
+        unset -- ${!_*}
+        IP=		# MUST [ip4] interface IPv4.  '-': script did it, 0.0.0.0 to ignore this MAC
+        _MASK=		# OPT: [mask] interface netmask, either /CIDR or 255.x.y.z
+        _GW=		# OPT: [ip4] default router
+        _BC=		# OPT: [ip4] Broadcast address, default taken from interface
+        _RENEW=		# OPT: [s] until RENEWING,  default  900000
+        _LEASE=		# OPT: [s] for LEASE time,  default 1000000
+        _REBIND=	# OPT: [s] until REBINDING, default 1500000
+        _TFTP=		# OPT: [ip4] TFTP, default: GW
+        _FILE=		# OPT: [path] BOOTP-Path
+        _SEED=		# OPT: [word] http://$GW/d-i/$SEED/preseed.cfg
+        _SECS=		# OPT: [s] seconds since boot
+        _FLAG=		# OPT: flags
+        _REPL=		# OPT: "port IP" or "0 IP" (for default port) reply to address
+        _HOSTNAME=	# OPT: hostname (DHCP 12)
+        _DOMAINNAME=	# OPT: domain name (DHCP 15)
+        _DNS4=		# OPT: list of IPv4 nameservers (DHCP 6)
+
+        . "$a" &&	# import request()
+        request "$@" &&	# run request()
+        [ -n "$IP" ] &&
+        break
+  done
+  case "$IP" in
+  (-)		exit;;	# script did everything
+  ('')		bye no matching VM found 'for' "$MAC";;
+  (0.0.0.0)	bye ignoring "$MAC";;
+  esac
+  echo "$IP" > "cache/$MAC.mac"
+
+  run	GW_J_R	ip -j r g "$IP"
+  run	GW_IP	jq -r '.[0].prefsrc' <<<"$GW_J_R"
+  run	GW_DEV	jq -r '.[0].dev' <<<"$GW_J_R"
+  run	GW_J_A	ip -j a s "$GW_DEV"
+  run	GW_CIDR	jq -r '.[].addr_info[] | select(.family=="inet").prefixlen' <<<"$GW_J_A"
+  def	MASK	"/$GW_CIDR"
+  def	GW	"$GW_IP"
+  def	TFTP	"$_GW"
+  def	SEED	''
+  def	FILE	"${_SEED:+http://$_GW/d-i/$_SEED/preseed.cfg}"
+  def	REPL	''
+  def	FLAG	0
+  def	SECS	0
+  def	RENEW	 900000
+  def	LEASE	1000000
+  def	REBIND	1500000
+  case "$_MASK" in
+  (/*)	bits=$[ 0xffffffff << (32 - "${_MASK#/}") ];
+        printf -v _MASK '%d.%d.%d.%d' $[ (bits>>24)&0xff ] $[ (bits>>16)&0xff ] $[ (bits>>8)&0xff ] $[ (bits>>0)&0xff ];
+        ;;
+  esac
+  IFS=. read a b c d u v w x <<<"$IP.$_MASK" && printf -v BCDEF %d.%d.%d.%d $[ a|(u^255) ] $[ b|(v^255) ] $[ c|(w^255) ] $[ d|(x^255) ]
+  def	BC	"$BCDEF"
+
+  # Perhaps augment a bit
+  [ -s "ip/$IP.sh" ] && . "ip/$IP.sh"
+
+  # Do some caching (for preseed.sh etc.)
+  for c in IP ${!_*}
+  do
+  	[ _ = "$c" ] || printf "%q=%q\n" "$c" "${!c}"
+  done > "cache/$IP.tmp" &&			# output cache/$IP.cache
+  mv -f "cache/$IP.tmp" "cache/$IP.ip" &&	# this hopefully is an atomic rename()
+  ln -fs "$IP.ip" "cache/$MAC.mac"
+}
+
+# If it is cached, reuse it a single time
+[ "cache/$MAC.mac" ] && . "cache/$MAC.mac" && rm -f "cache/$MAC.mac" || request
+
+arp >&2
 
 case "$DHCP53_bytes" in
 (01)	out DHCP 53 1 2;;	# DISCOVER => OFFER
 (03)	out DHCP 53 1 5;;	# REQUEST => ACK    -- we probably should be a bit more clever here
-(*)	out VEND 0; printf 'unsupported DHCP type %q\n' "$DHCP53_bytes" >&2; exit 0;;
+(*)	bye unsupported DHCP type "$DHCP53_bytes";;
 esac
-out DHCP 28 i "$BC"
-out DHCP 54 i "$GW"
-out DHCP  1 i "$MASK"		# => malformed according to tshark?
-out DHCP  3 i "$GW"		# works
-out DHCP 58 4 "$RENEW"
-out DHCP 51 4 "$LEASE"
-out DHCP 59 4 "$REBIND"
+
+out REPL "$_REPL"
+out ADDR "$IP"			# Set the IP of the VM
+out TFTP "$_TFTP"		# Set our interface as TFTP server
+out HOST "$_GW_IP"		# output BOOTP/DHCP server IP
+out FILE "$_FILE"		# output boot file
+out SECS "$_SECS"
+out FLAG "$_FLAG"
+
+out DHCP 12 s "$_HOSTNAME"
+out DHCP 15 s "$_DOMAINNAME"
+out DHCP  6 i "$_DNS4"
+out DHCP 28 i "$_BC"
+out DHCP 54 i "$_GW"
+out DHCP  1 i "$_MASK"		# => malformed according to tshark?
+out DHCP  3 i "$_GW"		# works
+out DHCP 58 4 "$_RENEW"
+out DHCP 51 4 "$_LEASE"
+out DHCP 59 4 "$_REBIND"
 out DHCP 255
 
-#sleep 5
 exit 0
 
