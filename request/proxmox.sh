1ca15cd05427acb721e4f90a79c281d0c52c9102
Valentin Hilbig <webmaster@scylla-charybdis.com>
2024-05-26 13:37:41 +0000

    request/proxmox.sh modified

--- request/proxmox.sh
+++ request/proxmox.sh
@@ -12,29 +12,51 @@
 
 CONFS=(/etc/pve/local/qemu-server/*.conf)
 
+# This runs as a subshell
 snapinfo()
 {
+  printf 'DEBUG: %q arg %q\n' "$@" >&2
+  if	[ 0 = "$1" ]
+  then
+	# $2 == 'current' here
+	# Dump _PROXMOX_ variables from $conf got in request()
+	mapfile PROXMOX <<<"$conf"
+	for p in "${PROXMOX[@]}"
+	do
+		pk="${p%%:*}"
+		pk="${pk%% *}"
+		case "$pk" in
+		(*[^a-z0-9A-Z_]*)	printf 'WTF %q: %q\n' "$0" "$p" >&2;;
+		([a-z]*)		printf '_PROXMOX_%s=%s\n' "${pk^^}" "${p#"$pk: "}"
+					printf 'DEBUG %q: %q %q\n' "$pk" "${p#"$pk: "}" >&2
+					;;
+		esac
+	done
+	return
+  fi
+
   conf="$(qm config "$VM" --snapshot "$2")" || return
   desc="${conf#*$'\ndescription: '}"	# extract description
   [ ".$conf" = ".$desc" ] && return	# not found
   desc="${desc%%$'\n'*}"		# remove trailing lines
-  printf '%b' "${desc//%/\\x}"		# decode it
+
+  printf '%b' "${desc//%/\\x}"		# decode description
 }
 snapnext()
 {
-  [ "${1:-0}" -le "$depth" ] && printf '%s' "${snap[$[depth-1-$1]]}"
+  [ "${1:-0}" -le "$depth" ] && printf '%s' "${snap[$[depth-$1]]}"
 }
 snapget()
 {
    snap=()
    while IFS='' read -r line
    do
-         depth="${line%%[^ ]*}"
-         depth="${#depth}"
-         line="${line#*'-> '}"
-         name="${line%% *}"
-         snap[$depth]="$name"
-         [ current = "$name" ] && return
+	depth="${line%%[^ ]*}"
+	depth="${#depth}"
+	line="${line#*'-> '}"
+	name="${line%% *}"
+	snap[$depth]="$name"
+	[ current = "$name" ] && return
    done < <(LC_ALL=C qm listsnapshot "$VM")
    return 1
 }
@@ -45,17 +67,17 @@ request()
 
   while read -ru6 VM
   do
-        VM="${VM##*/}"
-        VM="${VM%.*}"
+	VM="${VM##*/}"
+	VM="${VM%.*}"
 
-        conf="$(qm config "$VM" --current 1)"
-        case "${conf,,}" in
-        (*$'\nnet'*"=$MAC,"*)	;;
-        (*)			continue;;
-        esac
+	conf="$(qm config "$VM" --current 1)"
+	case "${conf,,}" in
+	(*$'\nnet'*"=$MAC,"*)	;;
+	(*)			continue;;
+	esac
 
-        snapget && walksnaps snapnext snapinfo snapnext proxmox VM "$VM"
-        return
+	snapget && walksnaps snapnext snapinfo snapnext proxmox VM "$VM"
+	return
   done 6< <(grep -il "^net.*=$MAC," "${CONFS[@]}")
   return 1
 }
